3e254e26a352768c1af4719e6ba629cd8a7b1f8f
nhicksn <nhicksn@umich.edu>
2022-10-06 16:12:53 -0400

    Pack.cpp done except shuffle

--- Pack.cpp
+++ Pack.cpp
@@ -12,7 +12,15 @@
 // NOTE: The standard order is the same as that in pack.in.
 // NOTE: Do NOT use pack.in in your implementation of this function
 Pack::Pack() {
-    assert(false);
+    int index = 0;
+    for(int suit = 0; suit < NUM_SUITS; suit++) {
+        for(int rank = 7; rank < NUM_RANKS; rank++) {
+            Card card = Card(RANK_NAMES_BY_WEIGHT[rank], SUIT_NAMES_BY_WEIGHT[suit]);
+            this->cards[index] = card;
+            index++;
+        }
+    }
+    this->next = 0;
 }
 
 // REQUIRES: pack_input contains a representation of a Pack in the
@@ -20,29 +28,41 @@ Pack::Pack() {
 // MODIFIES: pack_input
 // EFFECTS: Initializes Pack by reading from pack_input.
 Pack::Pack(std::istream& pack_input) {
-    assert(false);
+    std::string rank, temp, suit;
+    for(int i = 0; i < PACK_SIZE; i++) {
+        pack_input >> rank >> temp >> suit;
+        Card card = Card(rank, suit);
+        this->cards[i] = card;
+    }
+    this->next = 0;
 }
 
 // REQUIRES: cards remain in the Pack
 // EFFECTS: Returns the next card in the pack and increments the next index
 Card Pack::deal_one() {
-    assert(false);
+    Card card = cards[this->next];
+    this->next++;
+    return card;
 }
 
 // EFFECTS: Resets next index to first card in the Pack
 void Pack::reset() {
-  assert(false);
-  assert(next); // DELETEME: avoid error "private field is not used"
+  this->next = 0;
 }
 
 // EFFECTS: Shuffles the Pack and resets the next index. This
 //          performs an in shuffle seven times. See
 //          https://en.wikipedia.org/wiki/In_shuffle.
 void Pack::shuffle() {
-    assert(false);
+    for(int i = 0; i < 7; i++) {
+        
+    }
+    this->reset();
 }
 
 // EFFECTS: returns true if there are no more cards left in the pack
 bool Pack::empty() const {
+    if(this->next == PACK_SIZE) return true;
+    else return false;
     assert(false);
 }

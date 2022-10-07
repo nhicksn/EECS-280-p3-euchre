7333d2b848a2d720848d068b8577c3d2556b5596
nhicksn <nhicksn@umich.edu>
2022-10-07 16:37:28 -0400

    changes to player

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <array>
 #include "Player.h"
 
 Player * Player_factory(const std::string &name, const std::string &strategy) {
@@ -28,13 +29,46 @@ std::ostream & operator<<(std::ostream &os, const Player &p) {
 class SimplePlayer: public Player {
 private:
   std::string name;
+  std::array<Card, MAX_HAND_SIZE> hand;
 
 public:
-  SimplePlayer() {
-    this->name = "";
+  SimplePlayer(std::string &nameInput) {
+    this->name = nameInput;
   }
-  const std::string get_name() {
+
+  //EFFECTS returns player's name
+  const std::string &get_name() const override {
     return this->name;
+  }
+
+  //REQUIRES player has less than MAX_HAND_SIZE cards
+  //EFFECTS  adds Card c to Player's hand
+  void add_card(const Card &c) override {
+    for(int i = 0; i < MAX_HAND_SIZE; i++) {
+      // Checks if the space is empty, then changes that spot to the new card
+      // Breaks once a spot is found
+      if(hand[i].get_rank() == "null") {
+        hand[i] = c;
+        break;
+      }
+    }
+  }
+
+  //REQUIRES round is 1 or 2
+  //MODIFIES order_up_suit
+  //EFFECTS If Player wishes to order up a trump suit then return true and
+  //  change order_up_suit to desired suit.  If Player wishes to pass, then do
+  //  not modify order_up_suit and return false.
+  virtual bool make_trump(const Card &upcard, bool is_dealer,
+                          int round, std::string &order_up_suit) const override {
+    assert(round == 1 || round == 2);
+    int trumpCounter = 0;
+    std::string upTrump = upcard.get_suit(order_up_suit);
+    if(round == 1) {
+      for(int i = 0; i < MAX_HAND_SIZE; i++) {
+        
+      }
+    }
   }
 };
 
